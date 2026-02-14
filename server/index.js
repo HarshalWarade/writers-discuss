cbc11091ca13b2a1705d71d09e18e477e46c5478
harshalDev <harshalwarade344@gmail.com>
2026-02-14 14:22:37 +0530

    update cors policies

--- server/index.js
+++ server/index.js
@@ -1,7 +1,25 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const allowedOrigins = [
+  process.env.TESTING_FE_DOMAIN,
+  process.env.DEPLOYED_FE_DOMAIN,
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "Server is responding" });
