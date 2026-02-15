68f3130e2773ca9149e72dfb9709513643b045b5
harshalDev <harshalwarade344@gmail.com>
2026-02-15 15:08:05 +0530

    setup browserRouter and TailwindCSS

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,40 +1,15 @@
-import { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 const App = () => {
-  const [message, setMessage] = useState("Checking vibe...");
-
-  useEffect(() => {
-    const checkVibe = async () => {
-      try {
-        const response = await fetch(
-          `${import.meta.env.VITE_DEPLOYED_BE_DOMAIN}/vibe-check`,
-          {
-            method: "GET",
-            credentials: "include",
-          },
-        );
-
-        if (!response.ok) {
-          throw new Error("Failed to connect to backend");
-        }
-
-        const data = await response.json();
-        setMessage(data.message);
-      } catch (error) {
-        console.error("Error:", error);
-        setMessage("Backend not reachable ❌");
-      }
-    };
-
-    checkVibe();
-  }, []);
-
-  return (
-    <div>
-      <h2>Writers Discuss</h2>
-      <div>{message}</div>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
