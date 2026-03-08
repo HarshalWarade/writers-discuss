5ada190c6ae399cf42fd71555dab15491b4005c7
harshalDev <harshalwarade344@gmail.com>
2026-03-08 23:18:34 +0530

    add reward component with some modification to the exisiting color scheme

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,1 +1,1 @@
-export const COMMON_SPAN_CLASS = "uppercase font-bold text-red-label relative";
+export const COMMON_SPAN_CLASS = "uppercase font-bold text-indigo-800 relative";
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import HeroSection from "./sections/HeroSection";
+import MainOneSection from "./sections/MainOneSection";
 const Home = () => {
   return (
     <>
       <HeroSection />
-      {/* <MainOneSection /> */}
+      <MainOneSection />
     </>
   );
 };
